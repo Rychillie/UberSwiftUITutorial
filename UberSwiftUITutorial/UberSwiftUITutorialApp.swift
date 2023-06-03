f0f73c9146851078df0c46b8576b0affe7a8065a
Rychillie <rychillie@hotmail.com>
2023-06-03 18:54:31 -0300

    Initial Commit

---
+++ UberSwiftUITutorial/UberSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUITutorialApp.swift
+//  UberSwiftUITutorial
+//
+//  Created by Rychillie Umpierre de Oliveira on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
